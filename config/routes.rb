9a8ccc704756d8e257b4df8c0e4f01985f6adaf8
Austen Allred <austen.allred@gmail.com>
2013-12-09 19:13:18 -0700

    added a feleicia page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Pinteresting::Application.routes.draw do
   root "pages#home" #home page
   get "about" => "pages#about" #creates about_path
   get "contact" => "pages#contact" #creates contact_path
+  get "feleicia" => "pages#feleicia" #creates feleicia_path
   
 
   # The priority is based upon order of creation: first created -> highest priority.
