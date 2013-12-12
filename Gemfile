3103a31991262e94cee1cd5d8c100360fe53b995
Austen Allred <austen.allred@gmail.com>
2013-12-12 08:28:49 -0700

    added devise

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 1.2'
 
 gem 'bootstrap-sass', '~> 3.0.2.0'
+gem "devise", "~> 3.2.2"
 
 group :development, :test do
 	# sql - for heroku
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Pinteresting::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  #note to set this to your actual host
+config.action_mailer.default_url_options = { :host => 'austenprojects.com' }  
 end
