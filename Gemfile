2ee4912ddbddabe4e3c53e90c9d8c96ef6ca2b64
Austen Allred <austen.allred@gmail.com>
2013-12-17 08:21:15 -0700

    Add Amazon S3 for uploads to heroku

--- Gemfile
+++ Gemfile
@@ -10,6 +10,8 @@ gem 'rails', '4.0.1'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
+gem 'aws-sdk', '~> 1.30.0'
+
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,16 @@ Pinteresting::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
   #note to set this to your actual host
 config.action_mailer.default_url_options = { :host => 'austenprojects.com' }  
+
+
+  #sets paperclip to upload images to Amazon S3
+  # config/environments/production.rb
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 end
